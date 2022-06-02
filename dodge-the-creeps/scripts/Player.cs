b72227831f826b556f387e53957d0ed6cc79b68d
steffen-dc <steffen.de.coster@student.ehb.be>
2022-06-02 19:04:35 +0200

    add movement controls to Player

--- dodge-the-creeps/scripts/Player.cs
+++ dodge-the-creeps/scripts/Player.cs
@@ -14,9 +14,52 @@ public class Player : Area2D
         ScreenSize = GetViewportRect().Size;
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        var velocity = Vector2.Zero; // The player's movement vector. (by default, the player should not be moving)
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.x += 1;
+        }
+
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.x -= 1;
+        }
+
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.y += 1;
+        }
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.y -= 1;
+        }
+
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+
+
+        // check whether the player is moving so we can call play() or stop() on the AnimatedSprite.
+        if (velocity.Length() > 0)
+        {
+            // normalize speed for when moving diagonally (else the player moves faster diagonally)
+            velocity = velocity.Normalized() * Speed;
+            animatedSprite.Play();
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+
+        // update the player's position
+        // use clamp() to prevent it from leaving the screen. Clamping a value means restricting it to a given range.
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+            y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
+        );
+    }
 }
