180909fa3d03bc02f1b297273d6e4f0852506d61
steffen-dc <steffen.de.coster@student.ehb.be>
2022-06-03 20:34:51 +0200

    link all timer nodes Timeout signal to their method in Main script

--- dodge-the-creeps/scripts/Main.cs
+++ dodge-the-creeps/scripts/Main.cs
@@ -36,4 +36,42 @@ public class Main : Node
 
         GetNode<Timer>("StartTimer").Start();
     }
+
+    public void OnScoreTimerTimeout()
+    {
+        Score++;
+    }
+
+    public void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    public void OnMobTimerTimeout()
+    {
+        // Create a new instance of the Mob scene.
+        var mob = (Mob)MobScene.Instance();
+
+        // Choose a random location on Path2D.
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.Offset = GD.Randi();
+
+        // Set the mob's direction perpendicular to the path direction.
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        // Set the mob's position to a random location.
+        mob.Position = mobSpawnLocation.Position;
+
+        // Add some randomness to the direction.
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        // Choose the velocity.
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
 }
